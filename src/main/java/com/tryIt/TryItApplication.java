f29de46e143e65f975122adc943f1f68ebd3984f
keeeeeey <sseioul@naver.com>
2021-10-28 17:38:28 +0900

    Final Project

---
+++ src/main/java/com/tryIt/TryItApplication.java
@@ -0,0 +1,13 @@
+package com.tryIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tryIt/TryItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tryIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
