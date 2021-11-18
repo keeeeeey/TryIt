7dc93029430580d55676b2cc1ea027f33c86e09c
NohYeaJin <yeajinny@naver.com>
2021-11-18 15:36:39 +0900

    cart_ver1

--- src/main/java/com/tryIt/domain/NYJ_CartListVO.java
+++ src/main/java/com/tryIt/domain/NYJ_CartListVO.java
@@ -3,10 +3,13 @@ package com.tryIt.domain;
 import lombok.Data;
 
 @Data
-public class NYJ_CartVO {
+public class NYJ_CartListVO {
     private Long id;
     private Long user_id;
     private Long product_id;
     private Long product_num;
-
+    private String product_name;
+    private String product_img;
+    private Long product_price;
+    private int total_price;
 }
--- src/main/java/com/tryIt/service/NYJ_CartServiceImpl.java
+++ src/main/java/com/tryIt/service/NYJ_CartServiceImpl.java
@@ -1,4 +1,38 @@
 package com.tryIt.service;
 
-public class NYJ_CartServiceImpl {
+
+import com.tryIt.domain.NYJ_CartListVO;
+import com.tryIt.domain.NYJ_CartVO;
+import com.tryIt.mapper.NYJ_CartMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class NYJ_CartServiceImpl implements NYJ_CartService{
+
+    private final NYJ_CartMapper mapper;
+
+    @Override
+    public void insertCart(Long product_id, Long member_id, int product_num) {
+        mapper.insertCart(product_id,member_id,product_num);
+    }
+
+    @Override
+    public void deleteCart(Long cart_id) {
+        mapper.deleteCart(cart_id);
+    }
+
+    @Override
+    public List<NYJ_CartListVO> getCartList(Long member_id) {
+        return mapper.getCartList(member_id);
+    }
+
+    @Override
+    public void deleteALl(Long member_id) {
+        mapper.deleteALl(member_id);
+
+    }
 }
