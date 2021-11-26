73f0b4e162d94b16781e08a7d2eb24e236f89d85
NohYeaJin <yeajinny@naver.com>
2021-11-26 09:35:44 +0900

    addv2

--- src/main/java/com/tryIt/controller/MainController.java
+++ src/main/java/com/tryIt/controller/MainController.java
@@ -5,6 +5,7 @@ import com.tryIt.service.NYJ_ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
